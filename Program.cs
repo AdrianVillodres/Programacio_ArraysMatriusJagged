dc01db33467046cb5690d389b85bd244a2aea606
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2025-11-10 13:11:00 +0100

    code done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Arrays_Matrix_JaggedA
+{
+    public class Exercice
+    {
+        public static void Main()
+        {
+            const int size = 20;
+
+            Random rand = new Random();
+            int[] nums = new int[size];
+
+            for(int i = 0; i < size; i++)
+            {
+                nums[i] = rand.Next(1,100);
+            }
+            for (int i = 0; i < size; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    Console.Write($"{nums[i]} ");
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < size; i++)
+            {
+                if (nums[i] % 2 != 0)
+                {
+                    Console.Write($"{nums[i]} ");
+                }
+            }
+
+        }
+    }
+}
