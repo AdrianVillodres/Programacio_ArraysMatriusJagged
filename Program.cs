598ee22a23fefc334a2f87e8840177b659de88c2
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2025-11-10 14:01:16 +0100

    code done

--- Program.cs
+++ Program.cs
@@ -4,31 +4,96 @@
     {
         public static void Main()
         {
-            const int size = 20;
+            const string IntroMsg = "List pokemon = 1 | exit = 0";
+            const string ErrorMsg = "Error, you must put an integer number between 0 and 1";
+            const string PokedexMsg = "Your pokedex contains:";
+            const string PkdxErrorMsg = "Error, you must put an integer between 1 and 15";
+            const string ReleaseMsg = "Which pokemon you want to release? Enter the index: ";
+            const string PkmnReleasedMsg = "You released {0}";
+            const string PkmnReleasedEmptyMsg = "The index {0} is empty. There's no Pokémon in that slot to release.";
+            const string NoPkmn = "empty";
 
-            Random rand = new Random();
-            int[] nums = new int[size];
+            int op;
+            int index;
+            string[] pokemonList = new string[] { "Pikachu", "Charmander", "Squirtle", "Bulbasaur", "Eevee", "Jigglypuff", "Meowth", "Psyduck", "Snorlax", "Gengar", "Machop", "Magikarp", "Vulpix", "Onix", "Abra"};
 
-            for(int i = 0; i < size; i++)
+            Console.WriteLine(IntroMsg);
+            op = 2;
+            while(op != 0)
             {
-                nums[i] = rand.Next(1,100);
-            }
-            for (int i = 0; i < size; i++)
-            {
-                if (nums[i] % 2 == 0)
+                try
                 {
-                    Console.Write($"{nums[i]} ");
+                    op = Int32.Parse(Console.ReadLine());
                 }
-            }
-            Console.WriteLine();
-            for (int i = 0; i < size; i++)
-            {
-                if (nums[i] % 2 != 0)
+                catch (FormatException)
                 {
-                    Console.Write($"{nums[i]} ");
+                    Console.WriteLine(ErrorMsg);
                 }
-            }
+                catch (OverflowException)
+                {
+                    Console.WriteLine(ErrorMsg);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine(ErrorMsg);
+                }
 
+                switch (op)
+                {
+                    case 1:
+                        index = 0;
+                        Console.WriteLine(PokedexMsg);
+                        for (int i = 0; i < pokemonList.GetLength(0); i++)
+                        {
+                            Console.WriteLine(pokemonList[i]);
+                        }
+                        Console.WriteLine(ReleaseMsg);
+                        try
+                        {
+                            index = Int32.Parse(Console.ReadLine());
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine(ErrorMsg);
+                        }
+                        catch (OverflowException)
+                        {
+                            Console.WriteLine(ErrorMsg);
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine(ErrorMsg);
+                        }
+                        if(index > 0 && index < 16)
+                        {
+                            for (int i = 0; i < pokemonList.GetLength(0); i++)
+                            {
+                                if (i == index && pokemonList[i] != "empty")
+                                {
+                                    Console.WriteLine(PkmnReleasedMsg, pokemonList[i]);
+                                    pokemonList[i] = NoPkmn;
+                                }
+                                else
+                                {
+                                    Console.WriteLine(PkmnReleasedEmptyMsg, i);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine(PkdxErrorMsg);
+                        }
+                        break;
+                    case 0:
+
+                        break;
+                    default:
+                        Console.WriteLine(ErrorMsg);
+                        break;
+                }
+                
+            }
+            
         }
     }
 }
