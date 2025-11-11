56d1aa385864bddbcea1a18edbbf5501f63dffd2
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2025-11-11 12:25:30 +0100

    first version done

--- Program.cs
+++ Program.cs
@@ -4,7 +4,45 @@
     {
         public static void Main()
         {
-            Console.WriteLine("Hello, world");
+            const string GlobalPointsMsg = "Global total points of the tournament: {0}" ; 
+
+            Random rand = new Random();
+            int[][] scores = new int[4][];
+            scores[0] = new int[3];
+            scores[1] = new int[5];
+            scores[2] = new int[2];
+            scores[3] = new int[4];
+            string[] names = {"LunaBlade", "IronFist", "ShadowNova", "PixelMage"};
+            float[] avgs = new float[4];
+            float avg = 0f;
+            int totalPoints = 0;
+            int counter = 0;
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                for (int j = 0; j < scores[i].Length; j++)
+                {
+                    scores[i][j] = rand.Next(0, 101);
+                    totalPoints += scores[i][j];
+                    avg += scores[i][j];
+                    totalPoints += scores[i][j];
+                    counter++;
+                }
+                avgs[i] = avg / counter;
+                avg = 0;
+                counter = 0;
+            }
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.Write($"{names[i]}: ");
+                for (int j = 0; j < scores[i].Length; j++)
+                {
+                    Console.Write($"{scores[i][j]} "); 
+                }
+                Console.WriteLine($"-> Average: {avgs[i]}");
+            }
+            Console.WriteLine(GlobalPointsMsg, totalPoints);
         }
     }
 }
