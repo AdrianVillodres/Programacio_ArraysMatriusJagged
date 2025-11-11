1b114d24e88c8a9fe3362223833a3a387e74e9b2
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2025-11-11 13:05:43 +0100

    map done

--- Program.cs
+++ Program.cs
@@ -4,7 +4,46 @@
     {
         public static void Main()
         {
-            Console.WriteLine("Hello world");
+            const int ROWS = 10;
+            const int COLS = 10;
+            int treasureSecretNumber = 5;
+
+            string[,] map = new string[ROWS, COLS];
+            Random rand = new Random();
+            int randomNumber = 0;
+            bool isTeasure = false;
+
+            for(int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    randomNumber = rand.Next(1, 21);
+                    if(randomNumber == treasureSecretNumber)
+                    {
+                        map[i, j] = "*";
+                        isTeasure = true;
+                    }
+                    else
+                    {
+                        map[i, j] = "0";
+                    }
+                }
+            }
+            if (!isTeasure)
+            {
+                map[5, 4] = "*";
+            }
+
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write($"{map[i, j]} ");
+                }
+                Console.WriteLine("");
+            }
+
+
         }
     }
 }
