d532fd82ec2cbcb89d8c0462b175fdb3fc14b57e
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2025-11-13 10:42:06 +0100

    first version done

--- Program.cs
+++ Program.cs
@@ -1,48 +1,79 @@
-﻿namespace Arrays_Matrix_JaggedA
+﻿using System.Text;
+
+namespace Arrays_Matrix_JaggedA
 {
     public class Exercice
     {
         public static void Main()
         {
-            const int ROWS = 10;
-            const int COLS = 10;
-            int treasureSecretNumber = 5;
+            Console.OutputEncoding = Encoding.UTF8;
+            const int Rows = 5;
+            const int Columns = 5;
+            const string DefZone = "🛡️";
+            const string AtkZone = "⚔️";
+            const string Tower = "🗼";
+            const string Cave = "🕳️";
+            const string NeutralZone = "🌿";
+            const string ExtraResource = "💎";
+            const string EmptyZone = "➖";
 
-            string[,] map = new string[ROWS, COLS];
-            Random rand = new Random();
-            int randomNumber = 0;
-            bool isTeasure = false;
+            int[] tropes = {0,1,2,3,4,5,6,7,8,9};
+            string[,] map = new string[Rows, Columns];
+            string[][] regions = new string[3][];
+            regions[0] = new string[4];
+            regions[1] = new string[2];
+            regions[2] = new string[3];
 
-            for(int i = 0; i < map.GetLength(0); i++)
+            for(int i=0; i < tropes.GetLength(0); i++)
             {
-                for (int j = 0; j < map.GetLength(1); j++)
+                if (tropes[i]%2 == 0)
                 {
-                    randomNumber = rand.Next(1, 21);
-                    if(randomNumber == treasureSecretNumber)
+                    Console.Write($"{DefZone} ");
+                }
+                else
+                {
+                    Console.Write($"{AtkZone} ");
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for(int j = 0; j < map.GetLength(1); j++)
+                {
+                    if(i % 2 == 0 && j % 2 == 0)
                     {
-                        map[i, j] = "*";
-                        isTeasure = true;
+                        Console.Write($"{Tower} ");
+                    }else if(i % 2 != 0 && j % 2 != 0)
+                    {
+                        Console.Write($"{Cave} ");
                     }
                     else
                     {
-                        map[i, j] = "0";
+                        Console.Write($"{NeutralZone} ");
                     }
-                }
-            }
-            if (!isTeasure)
-            {
-                map[5, 4] = "*";
-            }
-
-            for (int i = 0; i < map.GetLength(0); i++)
-            {
-                for (int j = 0; j < map.GetLength(1); j++)
-                {
-                    Console.Write($"{map[i, j]} ");
                 }
                 Console.WriteLine("");
             }
+            Console.WriteLine("");
+            Console.WriteLine("");
+            //par buit impar extrasource
+            for (int i = 0; i < regions.Length; i++)
+            {
+                for (int j = 0; j < regions.Length; j++)
+                {
+                    if (i % 2 == 0 && j % 2 == 0)
+                    {
+                        Console.Write($"{ExtraResource} ");
+                    }
+                    else if (i % 2 != 0 && j % 2 != 0)
+                    {
+                        Console.Write($"{EmptyZone} ");
+                    }
 
+                }
+                Console.WriteLine("");
+            }
 
         }
     }
