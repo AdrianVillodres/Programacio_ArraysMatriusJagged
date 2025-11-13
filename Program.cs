4c173f5e04f291f587d3c32234de5f8e2b7ee9d5
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2025-11-13 11:57:31 +0100

    errors fixed

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace Arrays_Matrix_JaggedA
             const string ExtraResource = "💎";
             const string EmptyZone = "➖";
 
-            int[] tropes = {0,1,2,3,4,5,6,7,8,9};
+            int[] tropes = new int[Rows + Columns];
             string[,] map = new string[Rows, Columns];
             string[][] regions = new string[3][];
             regions[0] = new string[4];
@@ -26,7 +26,7 @@ namespace Arrays_Matrix_JaggedA
 
             for(int i=0; i < tropes.GetLength(0); i++)
             {
-                if (tropes[i]%2 == 0)
+                if (i % 2 == 0)
                 {
                     Console.Write($"{DefZone} ");
                 }
@@ -35,8 +35,10 @@ namespace Arrays_Matrix_JaggedA
                     Console.Write($"{AtkZone} ");
                 }
             }
+            
             Console.WriteLine("");
             Console.WriteLine("");
+            
             for (int i = 0; i < map.GetLength(0); i++)
             {
                 for(int j = 0; j < map.GetLength(1); j++)
@@ -53,22 +55,25 @@ namespace Arrays_Matrix_JaggedA
                         Console.Write($"{NeutralZone} ");
                     }
                 }
+
                 Console.WriteLine("");
             }
+
             Console.WriteLine("");
             Console.WriteLine("");
-            //par buit impar extrasource
+
+            
             for (int i = 0; i < regions.Length; i++)
             {
                 for (int j = 0; j < regions.Length; j++)
                 {
-                    if (i % 2 == 0 && j % 2 == 0)
-                    {
-                        Console.Write($"{ExtraResource} ");
-                    }
-                    else if (i % 2 != 0 && j % 2 != 0)
+                    if ((i + j) % 2 == 0)
                     {
                         Console.Write($"{EmptyZone} ");
+                    }
+                    else 
+                    {
+                        Console.Write($"{ExtraResource} ");
                     }
 
                 }
